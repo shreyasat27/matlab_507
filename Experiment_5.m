4fa532ded3a0b5fd6a9f7ebe42533c8d211efc20
Shreya Satsangi <87295264+shreyasat27@users.noreply.github.com>
2023-10-01 13:15:35 +0530

    Experiment-5

---
+++ Experiment_5.m
@@ -0,0 +1,9 @@
+%Draw the bar chart for the given data. Let us have an imaginary classroom with 10 students. We know the percent of marks obtained by these students are 75, 58, 90, 87, 50, 85, 92, 75, 60 and 95.
+data =[75, 58, 90, 87, 50, 85, 92, 75, 60, 95];
+
+%to create bar chart 
+
+figure;
+bar(data);
+
+%customise the chart 
