18e08fb87889aa964a8565481acdfca7efbf3bb3
Shreya Satsangi <87295264+shreyasat27@users.noreply.github.com>
2023-10-07 11:17:38 +0530

    Update experiment_7.m
    7. Create row vectors and column vectors, matrices (square and rectangular) . Also perform matrix addition,subtraction and multiplication.

--- experiment_7.m
+++ experiment_7.m
@@ -4,17 +4,29 @@
 
 r = [ 7 8 9 10 11];
 
+% Display the result
+disp(r);
+
 %column vectos
 
 c = [7; 8; 9; 10; 11];
+
+% Display the result
+disp(c);
 
 %square matrix
 
 a = [1 2 3 4; 2 3 4 5; 3 4 5 6; 4 5 6 7];
 
+% Display the result
+disp(a);
+
 %rectangular matrix
 
-a = [ 1 2 3 4; 2 3 4 5; 3 4 5 6; 4 5 6 7; 5 6 7 8];
+b = [ 1 2 3 4; 2 3 4 5; 3 4 5 6; 4 5 6 7; 5 6 7 8];
+
+% Display the result
+disp(b);
 
 %addition of matrix
 
